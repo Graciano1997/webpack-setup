a4dde7e4d81e7f774a76a12b39fd466838d89704
graciano1997 <gracianomanuelhenrique@gmail.com>
2023-07-19 17:23:39 +0100

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  mode:'development',
+  devtool: 'inline-source-map',
+  devServer:{
+    static:'./dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/indexTemplate.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean:true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
